e1e516dbcc6c747243f8b36833b6fa188e734438
Isuru sankhajith <isankhajith@gmail.com>
2023-10-29 10:19:23 +0530

    updated file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1>Hello world!</h1>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'project-1';
+  title = 'Tipsy Turtle';
 }
+
+
+                                 
